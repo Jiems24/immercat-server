eeb232b6d603a53ff8c105aa7402481b84139ad4
Jaume Barriuso <jaume.barriuso@gmail.com>
2026-04-15 23:45:58 +0200

    temp: disable CORS restriction for debugging

--- config/index.js
+++ config/index.js
@@ -22,8 +22,7 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors(()));
+  app.use(cors());
 
   // In development environment the app logs
   app.use(logger("dev"));
